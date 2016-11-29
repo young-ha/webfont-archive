78dd342cf11af0029d287ddfc9aaf377eef33f23
Seungha Kim <seungha.me@gmail.com>
2016-11-29 23:41:14 +0900

    css file, Youth

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pip install fonttools
-mkdir -p dist
-for p in ./source/*.*; do
+mkdir -p $DIR/dist
+for p in $DIR/source/*.*; do
   filename=$(basename "$p")
   title="${filename%.*}"
-  if [ -f ./dist/$title.woff ]; then
-    echo "$title.woff already exists."
+  if [ -f $DIR/dist/$title.woff ]; then
+    echo -e "\e[1m\e[32m$title.woff\e[0m already exists."
   else
-    pyftsubset "$p" --output-file="./dist/${title}.woff" --text-file=glyphs.txt --verbose --drop-tables= --layout-features='*' --desubroutinize --symbol-cmap --legacy-cmap --glyph-names --layout-features='' --hinting-tables='' --legacy-kern --passthrough-tables --name-IDs='*' --name-languages='*' --name-legacy
+    printf "Making \e[1m\e[34m$title.woff\e[0m..."
+    pyftsubset "$p" --output-file="$DIR/dist/${title}.woff" --text-file=$DIR/glyphs.txt --drop-tables= --layout-features='*' --desubroutinize --symbol-cmap --legacy-cmap --glyph-names --layout-features='' --hinting-tables='' --legacy-kern --passthrough-tables --name-IDs='*' --name-languages='*' --name-legacy
+    if [ $? ]; then
+      printf "done!\n"
+    fi
   fi
 done

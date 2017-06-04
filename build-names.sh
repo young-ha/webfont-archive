e7c7b8ea8a0427cf351b8709500fef2f323dd962
Kim Seungha <seungha.me@gmail.com>
2017-06-04 17:58:21 +0900

    fix bug (file existency check)

--- build-names.sh
+++ build-names.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p $DIR/dist/names
+DIST_DIR=$DIR/dist/names
+mkdir -p $DIST_DIR
 for p in $DIR/name-glyphs/*.*; do
   glyph_filename=$(basename "$p")
   font_filename=`echo $glyph_filename | perl -pe 's|(.+?)\.txt|\1|'`
   title="${font_filename%.*}"
   output_filename="${title}-name"
-  if [ -f $DIR/dist/$output_filename.woff ]; then
+  if [ -f $DIST_DIR/$output_filename.woff ]; then
     echo -e "\e[1m\e[32m$title.woff\e[0m already exists."
   else
     printf "Making \e[1m\e[34m$title.woff\e[0m..."
-    pyftsubset "$DIR/source/$font_filename" --output-file="$DIR/dist/names/$output_filename.woff" --text-file=$DIR/name-glyphs/$glyph_filename --drop-tables= --layout-features='*' --desubroutinize --symbol-cmap --legacy-cmap --glyph-names --layout-features='' --hinting-tables='' --legacy-kern --passthrough-tables --name-IDs='*' --name-languages='*' --name-legacy
+    pyftsubset "$DIR/source/$font_filename" --output-file="$DIST_DIR/$output_filename.woff" --text-file=$DIR/name-glyphs/$glyph_filename --drop-tables= --layout-features='*' --desubroutinize --symbol-cmap --legacy-cmap --glyph-names --layout-features='' --hinting-tables='' --legacy-kern --passthrough-tables --name-IDs='*' --name-languages='*' --name-legacy
     if [ $? ]; then
       printf "done!\n"
     fi
